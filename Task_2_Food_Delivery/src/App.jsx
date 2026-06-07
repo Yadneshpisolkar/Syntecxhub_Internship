a9be62a6fe17906bf50321fb88446e91b03b4ec7
Yadneshpisolkar <pisolkaryadnesh@gmail.com>
2026-06-07 11:02:41 +0530

    Added Task 1 and Task 2

---
+++ Task_2_Food_Delivery/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
